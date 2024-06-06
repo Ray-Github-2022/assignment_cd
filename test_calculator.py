ea1ec23a7001a15d94f8afad8bd619a41260cdb3
RAY <118811800+Ray-Github-2022@users.noreply.github.com>
2024-06-06 16:21:40 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -10,19 +10,19 @@ def client():
         yield client
 
 def test_add(client):
-    response = client.get('/add?a=10&b=5')
+    response = client.get('/?a=10&b=5&operation=add')  # Updated route here
     json_data = response.get_json()
     assert response.status_code == 200
     assert json_data['result'] == 15
     print(f"Test addition: 10 + 5 = {json_data['result']}")
 
-    response = client.get('/add?a=-1&b=1')
+    response = client.get('/?a=-1&b=1&operation=add')  # Updated route here
     json_data = response.get_json()
     assert response.status_code == 200
     assert json_data['result'] == 0
     print(f"Test addition: -1 + 1 = {json_data['result']}")
 
-    response = client.get('/add?a=-1&b=-1')
+    response = client.get('/?a=-1&b=-1&operation=add')  # Updated route here
     json_data = response.get_json()
     assert response.status_code == 200
     assert json_data['result'] == -2
