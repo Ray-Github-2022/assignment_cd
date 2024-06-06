a506c8cb80db7ee96c2a1c0c58041eb5b757dd14
RAY <118811800+Ray-Github-2022@users.noreply.github.com>
2024-06-06 15:16:14 +0200

    Update test_lib.py

--- test_lib.py
+++ test_lib.py
@@ -1,4 +1,31 @@
-import lib
-def test_power():
-    assert lib.power(2,2) == 4
-    assert lib.power(2,3) == 8
+# test_calculation.py
+import pytest
+from WINC.python.assignment_cd.calculate import calculate
+
+@pytest.fixture
+def client():
+    calculate.config['TESTING'] = True
+    with calculate.test_client() as client:
+        yield client
+
+def test_add(client):
+    response = client.get('/add?a=10&b=5')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['result'] == 15
+    print(f"Test addition: 10 + 5 = {json_data['result']}")
+
+    response = client.get('/add?a=-1&b=1')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['result'] == 0
+    print(f"Test addition: -1 + 1 = {json_data['result']}")
+
+    response = client.get('/add?a=-1&b=-1')
+    json_data = response.get_json()
+    assert response.status_code == 200
+    assert json_data['result'] == -2
+    print(f"Test addition: -1 + -1 = {json_data['result']}")
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
